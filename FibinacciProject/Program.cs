fbb6baa59be17d9348324144ced69b565041a94d
gpdoud <gpdoud@gmail.com>
2017-06-05 16:09:01 -0400

    initial commit

---
+++ FibinacciProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FibinacciProject {
+
+    class Program {
+
+        void Run1() {
+            Random random = new Random();
+            for (int idx = 0; idx < 10; idx++) {
+                int aRandomNumber = random.Next(1, 11);
+                Debug.WriteLine(aRandomNumber);
+            }
+        }
+
+        void Run() {
+            int i = 1;
+            int j = 2;
+            while(true) {
+                var k = i + j;
+                i = j;
+                j = k;
+                if(i > 19000 && j > 19000) {
+                    Console.WriteLine(i.ToString() + " and " + j.ToString());
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+
+        static void Main(string[] args) {
+            (new Program()).Run1();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Fibinacci Numbers
+
+Displays the next two fibinacci number higher than 19,000
